8670721e43e867c5142d5be958998b4f222bd5e8
Sahil Ranpariya <sahilranpariya60@gmail.com>
2024-12-10 17:45:42 +0530

    feat/add redis client for session storing
    adding redis store client to save session data in it so whenever server is refreshed session data remain as it is

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import router from "./indexRoutes.js";
 import env from "./config.js";
 import cors from "cors";
 import errorCodes from "./errorCodes.js";
+import { createClient } from "redis";
+import { RedisStore } from "connect-redis";
 
 mongoose
   .connect(env.MY_DB_URL)
@@ -25,12 +27,26 @@ app.use(
   })
 );
 
+const client = createClient({
+  username: "default",
+  password: "yhHO3kbMTR7U6ABXnHRPDSnbPRGZh6OP",
+  socket: {
+    host: "redis-16280.c264.ap-south-1-1.ec2.redns.redis-cloud.com",
+    port: 16280,
+  },
+});
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+client.on("connect", () => console.log("Redis Client Connected Successfully"));
+await client.connect();
+
 app.use(
   session({
     secret: env.SESSION_KEY,
     resave: false,
     saveUninitialized: false,
     cookie: { httpOnly: true, secure: false, maxAge: 24 * 60 * 60 * 1000 }, // 1 day max age
+    store: new RedisStore({ client }),
   })
 );
 
